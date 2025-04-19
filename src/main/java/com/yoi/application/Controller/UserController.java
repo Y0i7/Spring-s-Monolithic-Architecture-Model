03f61e1c63daa62821749381b30279cba938a55f
Y0i7 <oryees27@gmail.com>
2025-04-19 11:42:27 -0500

    Updated
    Co-Authored-By: 但丁 <185263751+D4NT3-Dev@users.noreply.github.com>
    Co-Authored-By: []rlando (Y0i) <oryees27@gmail.com>

--- src/main/java/com/yoi/application/Controller/UserController.java
+++ src/main/java/com/yoi/application/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.yoi.application.Controller;
 import com.yoi.application.Model.User;
 import com.yoi.application.Service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,6 +21,7 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
     private final UserService userService;
 
     public UserController(UserService userService) {
@@ -28,8 +30,8 @@ public class UserController {
 
     @GetMapping
     public String getAllUsers(Model model) {
-        List<User> user = userService.getAllUsers();
-        model.addAttribute("users", user);
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
         return "users/list";
     }
 
